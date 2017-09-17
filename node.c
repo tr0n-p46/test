f830904bdff5fda8bacfb0b456ce5b842d31576a
tr0n-p46 <souvikd197@gmail.com>
2017-09-17 13:20:47 +0530

    character count

--- node.c
+++ node.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 
 void main() {
-	int c;	
-	while((c=getchar())!=EOF) putchar(c);
+	int blank=0,tab=0,newline=0;
+	int c;
+	while((c=getchar())!=EOF) {
+		switch(c) {
+			case ' ': ++blank;
+				  break;
+			case '\n': ++newline;
+				  break;
+			case '\t': ++tab;
+				  break;		
+		}	
+		if(c=='q') break;
+	}
+	printf("Blank : %d\nTab : %d\nNew Line : %d\n",blank,tab,newline);
 }
