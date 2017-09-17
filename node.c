b1ff2e2ccd1d50ec1cda6f306082aad3fb853313
tr0n-p46 <souvikd197@gmail.com>
2017-09-17 12:27:59 +0530

    Fahrenheit to Celsius

--- node.c
+++ node.c
@@ -1,4 +1,13 @@
 #include <stdio.h>
+
+#define STEP   20
+#define LOW     0
+#define UPPER 300
+
+#define F2C(f) ((5/9.0)*((f)-32))
+
 void main() {
-printf("Hello! World");
+	int f;
+	for(f=LOW;f<=UPPER;f+=STEP) 
+		printf("%3d %6.1f\n",f,F2C(f));	
 }
