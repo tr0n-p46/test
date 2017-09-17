a859089d7f50719337739248e7d3a6f7b32b7a90
tr0n-p46 <souvikd197@gmail.com>
2017-09-17 18:22:26 +0530

    longest line count

--- node.c
+++ node.c
@@ -1,18 +1,37 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-void main() {
-	int blank=0,tab=0,newline=0;
-	int c;
-	while((c=getchar())!=EOF) {
-		switch(c) {
-			case ' ': ++blank;
-				  break;
-			case '\n': ++newline;
-				  break;
-			case '\t': ++tab;
-				  break;		
-		}	
-		if(c=='q') break;
+#define MAXLINE 100
+
+int getLine(FILE*,char[]);
+void copy(char[],char[]);
+
+int main() {
+	FILE *fp=fopen("hello.txt","r");
+	int len,max=0;
+	char line[MAXLINE],longest[MAXLINE];
+	while((len=getLine(fp,line))>0) {
+		if(len>max) {
+			max=len;
+			copy(line,longest);		
+		}
 	}
-	printf("Blank : %d\nTab : %d\nNew Line : %d\n",blank,tab,newline);
+	if(max>0)
+		printf("%s (%d)\n",longest,max);	
+	fclose(fp);
+}
+
+int getLine(FILE *fp,char line[]) {
+	int c;
+	int count=0;
+	while((c=fgetc(fp))!=EOF && c!='\n') {
+		line[count]=c;
+		++count;		
+	}
+	return count;
+}
+
+void copy(char line[],char longest[]) {
+	int i=0;
+	while(line[i]!='\0') longest[i]=line[i++];
 }
