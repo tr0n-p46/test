3258cb9caf435810f5039d2ff9703aea5d40c33f
tr0n-p46 <souvikd197@gmail.com>
2017-09-17 12:51:46 +0530

    file copy

--- node.c
+++ node.c
@@ -1,13 +1,6 @@
 #include <stdio.h>
 
-#define STEP   20
-#define LOW     0
-#define UPPER 300
-
-#define F2C(f) ((5/9.0)*((f)-32))
-
 void main() {
-	int f;
-	for(f=LOW;f<=UPPER;f+=STEP) 
-		printf("%3d %6.1f\n",f,F2C(f));	
+	int c;	
+	while((c=getchar())!=EOF) putchar(c);
 }
